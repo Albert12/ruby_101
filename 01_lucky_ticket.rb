31ab8b1e91deece97a5fbdf5905bce4ff3e29e15
Albert_12 <albert.postal@gmail.com>
2013-02-26 20:09:14 +0400

    2 files changed

--- 01_lucky_ticket.rb
+++ 01_lucky_ticket.rb
@@ -4,11 +4,11 @@ lucky_tickets_qty = 0
 for counter in lucky_tickets_start_number..lucky_tickets_end_number do
 	ticket_number = "%06d" % counter
 	left, right  = 0, 0
-	ticket_number[0..2].chars.map {|c| c.to_i}.each { |d| left+=d }
-	ticket_number[3..5].chars.map {|c| c.to_i}.each { |d| right+=d }
+	ticket_number[0..2].chars.map {|c| c.to_i}.each {|d| left+=d}
+	ticket_number[3..5].chars.map {|c| c.to_i}.each {|d| right+=d}
 		if left == right then
 			lucky_tickets_qty +=1
 		end
 end
 puts "lucky tickets quantity is " + lucky_tickets_qty.to_s
-puts "quantity of lucky tickets is " + ("%.4f" % (100.to_f * lucky_tickets_qty / (lucky_tickets_end_number + 1))) + "% of the total quantity of tickets"
+puts "quantity of lucky tickets is " + ("%.4f" % (100.0 * lucky_tickets_qty / (lucky_tickets_end_number + 1))) + "% of the total quantity of tickets"

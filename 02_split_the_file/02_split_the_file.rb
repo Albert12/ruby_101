693b11a35be7700db4a6441b7562b9667f089e49
Albert_12 <albert.postal@gmail.com>
2013-02-27 16:51:23 +0400

    1 more update

--- 02_split_the_file/02_split_the_file.rb
+++ 02_split_the_file/02_split_the_file.rb
@@ -1,7 +1,5 @@
-file_name  = "r_reflex.txt"
-a, b = file_name.split('.')
-a.to_s
-b.to_s
-file = IO.read(file_name).force_encoding("KOI8-R").encode("utf-8", replace: nil)
+FILE_NAME  = "r_reflex.txt"
+a, b = FILE_NAME.split('.')
+file = IO.read(FILE_NAME).force_encoding("KOI8-R")
 arr = file.scan (/.{1,10240}/m)
 arr.each_index { |i| File.open(a + i.to_s + '.' +b, 'w') { |f| f.write(arr[i]) } }

9277c6484ffeddb4993b37bca03017565dabe220
Albert_12 <albert.postal@gmail.com>
2013-02-27 15:14:37 +0400

    current version is shorter and use regex

--- 02_split_the_file/02_split_the_file.rb
+++ 02_split_the_file/02_split_the_file.rb
@@ -1,16 +1,7 @@
 file_name  = "r_reflex.txt"
-block_size = 10240.0 # 1 kB=1024 B, 10kB=10240 B
-
 a, b = file_name.split('.')
 a.to_s
 b.to_s
-
-file = IO.read(file_name)
-file_size = file.size
-steps = (file.size / block_size).ceil - 1
-
-for i in 0..steps do
-	new_file = file[(i * block_size)..(i.next * block_size - 1)]
-	new_file_name = a + i.to_s + '.' +b
-	File.open(new_file_name, 'w') {|f| f.write(new_file) }
-end
+file = IO.read(file_name).force_encoding("KOI8-R").encode("utf-8", replace: nil)
+arr = file.scan (/.{1,10240}/m)
+arr.each_index { |i| File.open(a + i.to_s + '.' +b, 'w') { |f| f.write(arr[i]) } }
